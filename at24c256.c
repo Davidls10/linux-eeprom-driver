8343f46c75488e999ba5d03e1b0102bf6651769f
David Lima <davidls10203040@gmail.com>
2024-09-15 14:00:28 -0300

    Char driver

--- at24c256.c
+++ at24c256.c
@@ -2,12 +2,133 @@
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
+<<<<<<< HEAD
+=======
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/err.h>
+>>>>>>> 0668391 (Char driver)
 
 #define I2C_BUS_AVAILABLE 1
 #define DEVICE_ADDRESS 0x50
+#define DEVICE_MEM_SIZE 32768
+
+dev_t device_number;
+struct cdev at24c256_cdev;
+struct class *class_at24c256;
+struct device *device_at24c256;
 
 static struct i2c_adapter *at24c256_i2c_adapter = NULL;
 static struct i2c_client *at24c256_i2c_client = NULL;
+
+static int at24c256_open(struct inode *inode, struct file *file);
+static int at24c256_release(struct inode *inode, struct file *file);
+static ssize_t at24c256_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t at24c256_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+
+static int sequential_read(unsigned char *buffer, unsigned char *read_data, unsigned int length);
+static int page_write(unsigned char *buffer, unsigned int len);
+
+struct file_operations at24c256_fops = {
+	.owner = THIS_MODULE,
+	.read = at24c256_read,
+	.write = at24c256_write,
+	.open = at24c256_open,
+	.release = at24c256_release
+};
+
+static int at24c256_open(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+static int at24c256_release(struct inode *inode, struct file *file) {
+	return 0;
+}
+
+static ssize_t at24c256_read(struct file *filp, char __user *buf, size_t len, loff_t *f_pos) {
+	printk(KERN_INFO "Read requested for %zu bytes \n", len);
+	if ((*f_pos + len) > DEVICE_MEM_SIZE - 1)
+		len = (DEVICE_MEM_SIZE - 1) - *f_pos;
+
+	unsigned char buf_sequential_read[2] = {0b00000001, 0b00001111};
+	unsigned char sequential_read_data[len];
+	sequential_read(buf_sequential_read, sequential_read_data, len);
+
+	if (copy_to_user(buf, sequential_read_data, len)) {
+		return -EFAULT;
+	}
+	*f_pos += len;
+
+	return len;
+}
+
+static ssize_t at24c256_write(struct file *filp, const char *buf, size_t len, loff_t *off) {
+	if ((*off + len) > DEVICE_MEM_SIZE - 1)
+		len = DEVICE_MEM_SIZE - 1 - *off;
+
+	unsigned char buf_page_write[len];
+	unsigned char buf_page_write_addr[len];
+
+	if (copy_from_user(buf_page_write, buf, len)) {
+		return -EFAULT;
+	}
+
+	*off += len;
+
+	buf_page_write_addr[0] = 0b00000001;
+	buf_page_write_addr[1] = 0b00001111;
+	for (int i = 0; i < len; i++) {
+		buf_page_write_addr[i+2] = buf_page_write[i];
+	}
+	page_write(buf_page_write_addr, len+2);
+
+	return len;
+}
+
+static int init_file(void) {
+	if ((alloc_chrdev_region(&device_number, 0, 1, "at24c256_devices") < 0)) {
+		printk(KERN_ALERT "Cannot allocate major number\n");
+		return -1;
+	}
+	printk(KERN_INFO "Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
+
+	at24c256_cdev.owner = THIS_MODULE;
+	cdev_init(&at24c256_cdev, &at24c256_fops);
+	if ((cdev_add(&at24c256_cdev, device_number, 1) < 0)) {
+		printk(KERN_ALERT "Cannot add the device to the system\n");
+		goto r_class;
+	}
+
+	if (IS_ERR(class_at24c256 = class_create("at24c256_class"))) {
+		printk(KERN_ALERT "Cannot create the struct class\n");
+		goto r_class;
+	}
+
+	if (IS_ERR(device_at24c256 = device_create(class_at24c256, NULL, device_number, NULL, "at24c256"))) {
+		printk(KERN_ALERT "Cannot create the device 1\n");
+		goto r_device;
+	}
+
+	printk(KERN_INFO "Device driver insert done!");
+
+	return 0;
+
+	r_device:
+		class_destroy(class_at24c256);
+	r_class:
+		unregister_chrdev_region(device_number, 1);
+		return -1;
+}
+
+static void deinit_file(void) {
+	device_destroy(class_at24c256, device_number);
+	class_destroy(class_at24c256);
+	cdev_del(&at24c256_cdev);
+	unregister_chrdev_region(device_number, 1);
+	printk(KERN_INFO "Module unloaded\n");
+}
 
 static int current_address_read(unsigned char *read_data) {
 	int ret = i2c_master_recv(at24c256_i2c_client, read_data, 1);
@@ -172,12 +293,16 @@ static int __init at24c256_driver_init(void) {
 	current_address_read(&data);
 	printk("Received current address data: %d\n", data);
 
+	printk(KERN_INFO "Init file return: %d\n", init_file());
+
 	return ret;
 }
 
 static void __exit at24c256_driver_exit(void) {
         i2c_unregister_device(at24c256_i2c_client);
         i2c_del_driver(&at24c256_driver);
+
+	deinit_file();
 
         printk(KERN_ALERT "AT24C256 driver removed");
 }
